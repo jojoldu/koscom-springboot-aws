a1b45148712fc1ea6a3c34f860e609b1646daa59
jojoldu <jojoldu@gmail.com>
2021-09-04 21:15:43 +0900

    초기 환경

--- src/main/java/com/koscom/springboot/Application.java
+++ src/main/java/com/koscom/springboot/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KoscomSpringbootAwsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KoscomSpringbootAwsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
