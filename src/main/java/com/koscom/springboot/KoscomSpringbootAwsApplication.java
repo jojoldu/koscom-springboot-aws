9ede7d21ffb90f42128ea7d9669008a412671609
jojoldu <jojoldu@gmail.com>
2021-09-04 17:10:21 +0900

    Initial commit

---
+++ src/main/java/com/koscom/springboot/KoscomSpringbootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.koscom.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoscomSpringbootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoscomSpringbootAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koscom/springboot/KoscomSpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koscom.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoscomSpringbootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
